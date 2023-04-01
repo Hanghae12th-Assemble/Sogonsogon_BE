669c5883480a784572865ec4bf66cc8840217352
colleenInKorea <92035417+colleenInKorea@users.noreply.github.com>
2023-04-02 05:30:36 +0900

    docs: sogonsogonapplication 수정

--- src/main/java/com/sparta/sogonsogon/SogonSgonApplication.java
+++ src/main/java/com/sparta/sogonsogon/SogonSgonApplication.java
@@ -1,11 +1,14 @@
 package com.sparta.sogonsogon;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@OpenAPIDefinition(servers = {@Server(url = "https://sogonsogon.shop", description = "Default Server URL") ,@Server(url = "http://localhost:8080", description = "Local Server URL")})
 public class SogonSgonApplication {
 
     public static void main(String[] args) {
