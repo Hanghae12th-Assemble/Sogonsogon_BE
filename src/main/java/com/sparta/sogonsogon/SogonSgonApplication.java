115a85d816e4aa76afeb1f60ec2781dbc381f0f9
idogyeong <ledgy98@gmail.com>
2023-04-01 13:58:10 +0900

    Initial commit

---
+++ src/main/java/com/sparta/sogonsogon/SogonSgonApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.sogonsogon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SogonSgonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SogonSgonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/sogonsogon/SogonSgonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.sogonsogon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SogonSgonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
