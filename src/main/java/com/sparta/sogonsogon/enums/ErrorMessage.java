784eaf8993ed1c7a4452c338f97b3d64214cbbae
colleenInKorea <92035417+colleenInKorea@users.noreply.github.com>
2023-04-03 02:47:23 +0900

    docs: ErrorMessage 메세지 추가

--- src/main/java/com/sparta/sogonsogon/enums/ErrorMessage.java
+++ src/main/java/com/sparta/sogonsogon/enums/ErrorMessage.java
@@ -26,6 +26,9 @@ public enum ErrorMessage {
     NOT_FOUND_AUDIOCLIP("해당 오디오 클립을 찾을 수 없습니다."),
     WRONG_FILE_TYPE("파일의 타입이 잘못되었습니다. "),
 
+    //audioclip 안에 있는 comment
+    NOT_FOUND_COMMENT("해당 오디오 클립에 있는 댓글을 찾을 수 없습니다."),
+
 
     // category
     NOT_FOUND_CATEGORY("해당하는 카테고리가 없습니다.")
