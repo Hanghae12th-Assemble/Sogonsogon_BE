bab9e2e1b75f0f38f1f5e8946b45202bbecae169
colleenInKorea <92035417+colleenInKorea@users.noreply.github.com>
2023-04-03 01:19:02 +0900

    docs: AudioClipService update 추가

--- src/main/java/com/sparta/sogonsogon/audioclip/service/AudioClipService.java
+++ src/main/java/com/sparta/sogonsogon/audioclip/service/AudioClipService.java
@@ -6,7 +6,9 @@ import com.sparta.sogonsogon.audioclip.entity.AudioClip;
 import com.sparta.sogonsogon.audioclip.repository.AudioClipRepository;
 import com.sparta.sogonsogon.dto.StatusResponseDto;
 import com.sparta.sogonsogon.enums.ErrorMessage;
+import com.sparta.sogonsogon.member.dto.MemberResponseDto;
 import com.sparta.sogonsogon.member.entity.Member;
+import com.sparta.sogonsogon.member.entity.MemberRoleEnum;
 import com.sparta.sogonsogon.member.repository.MemberRepository;
 import com.sparta.sogonsogon.security.UserDetailsImpl;
 import com.sparta.sogonsogon.util.S3Uploader;
@@ -31,6 +33,23 @@ public class AudioClipService {
         AudioClip audioClip = new AudioClip(requestDto, member);
         audioClipRepository.save(audioClip);
         return StatusResponseDto.success(HttpStatus.OK, new AudioClipResponseDto(audioClip));
+    }
+
+    @Transactional
+    public StatusResponseDto<AudioClipResponseDto> updateAudioClip(Long audioclipId, AudioClipRequestDto requestDto, UserDetailsImpl userDetails){
+        Member member = memberRepository.findById(userDetails.getUser().getId()).orElseThrow(
+                ()-> new IllegalArgumentException(ErrorMessage.WRONG_USERNAME.getMessage())
+        );
+        AudioClip audioClip = audioClipRepository.findById(audioclipId).orElseThrow(
+                ()-> new IllegalArgumentException(ErrorMessage.NOT_FOUND_AUDIOCLIP.getMessage())
+        );
+
+        if (member.getRole() == MemberRoleEnum.USER  || member.getMembername().equals(userDetails.getUser().getMembername())) {
+            audioClip.update(requestDto);
+            return StatusResponseDto.success(HttpStatus.OK, new AudioClipResponseDto(audioClip));
+        } else {
+            throw new IllegalArgumentException(ErrorMessage.ACCESS_DENIED.getMessage());
+        }
     }
 
 
