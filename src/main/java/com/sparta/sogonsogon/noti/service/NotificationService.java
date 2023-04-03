b9dc76ac06573317e6bb5ecdddff7398a2183161
Shanyu Li <uhaha529@gmail.com>
2023-04-03 20:09:08 +0900

    docs: 알림구독 유지 시간을 10분->60분으로 변경

--- src/main/java/com/sparta/sogonsogon/noti/service/NotificationService.java
+++ src/main/java/com/sparta/sogonsogon/noti/service/NotificationService.java
@@ -28,7 +28,7 @@ public class NotificationService {
     private final NotificationRepository notificationRepository;
 
     private final MemberRepository memberRepository;
-    private static final Long DEFAULT_TIMEOUT = 60 * 10000L;
+    private static final Long DEFAULT_TIMEOUT =  60 * 60000L;
 
     public SseEmitter subscribe(Long memberId) {
         Member member = memberRepository.findById(memberId)
