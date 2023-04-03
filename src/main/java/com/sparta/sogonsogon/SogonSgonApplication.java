bf025377a79be6049fc5ef85956a8402608feed7
colleenInKorea <92035417+colleenInKorea@users.noreply.github.com>
2023-04-03 20:18:31 +0900

    design : 내용 수정

--- src/main/java/com/sparta/sogonsogon/SogonSgonApplication.java
+++ src/main/java/com/sparta/sogonsogon/SogonSgonApplication.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-@OpenAPIDefinition(servers = {@Server(url = "https://sogonsogon.shop", description = "Default Server URL") ,@Server(url = "http://localhost:8080", description = "Local Server URL")})
+@OpenAPIDefinition(servers = {@Server(url = "https://sogonsogon.shop", description = "Default Server URL") ,@Server(url = "http://localhost:8080", description = "Local Server URL"),@Server(url= "http://52.79.151.2:8080", description = "http서버 입니다. ")})
 public class SogonSgonApplication {
 
     public static void main(String[] args) {
