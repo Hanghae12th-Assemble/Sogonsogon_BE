933167420e846098c197a9a82606cbfbd2f6591a
Shanyu Li <uhaha529@gmail.com>
2023-04-08 01:19:05 +0900

    docs: 알림기능 수정 테스트

--- src/main/java/com/sparta/sogonsogon/SogonSgonApplication.java
+++ src/main/java/com/sparta/sogonsogon/SogonSgonApplication.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 //@SpringBootApplication
 //@EnableJpaAuditing
-//@OpenAPIDefinition(servers = {@Server(url = "https://sogonsogon.shop", description = "Default Server URL") ,@Server(url = "http://localhost:8080", description = "Local Server URL"),@Server(url= "http://52.79.151.2:8080", description = "http서버 입니다. ")})
+//@OpenAPIDefinition(servers = {@Server(url = "http://localhost:8080", description = "Local Server URL"),@Server(url= "http://13.124.100.26:8080", description = "http서버 입니다. ")})
 //public class SogonSgonApplication {
 //
 //    public static void main(String[] args) {
@@ -18,7 +18,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 //}
 @SpringBootApplication
 @EnableJpaAuditing
-@OpenAPIDefinition(servers = {@Server(url = "http://localhost:8080", description = "Local Server URL"),@Server(url= "http://13.124.100.26:8080", description = "http서버 입니다. ")})
+@OpenAPIDefinition(servers = {@Server(url = "http://localhost:8080", description = "Local Server URL"),@Server(url= "https://13.124.100.26:8080", description = "http서버 입니다. ")})
 public class SogonSgonApplication {
 
     public static void main(String[] args) {
