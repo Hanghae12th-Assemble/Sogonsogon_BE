e9a84d6b4610379b36533cedca2e3e41607acb4c
colleenInKorea <92035417+colleenInKorea@users.noreply.github.com>
2023-04-08 06:02:00 +0900

    Test : cI/CD test

--- src/main/java/com/sparta/sogonsogon/util/S3Uploader.java
+++ src/main/java/com/sparta/sogonsogon/util/S3Uploader.java
@@ -65,11 +65,20 @@ public class S3Uploader {
     private Optional<File> convert(MultipartFile file) throws IOException {
         File convertFile = new File(System.getProperty("user.dir") + "/" + file.getOriginalFilename());
         log.info(convertFile.toString());
-        convertFile.getParentFile().mkdir();
-        convertFile.createNewFile();// 바로 위에서 지정한 경로에 File이 생성됨 (경로가 잘못되었다면 생성 불가능)
-        FileOutputStream fos = new FileOutputStream(convertFile);// FileOutputStream 데이터를 파일에 바이트 스트림으로 저장하기 위함
-        fos.write(file.getBytes());
 
+// 부모 디렉토리가 없으면 생성
+        if (!convertFile.getParentFile().exists()) {
+            convertFile.getParentFile().mkdirs();
+        }
+
+// 파일이 이미 존재하지 않으면 생성
+        if (!convertFile.exists()) {
+            convertFile.createNewFile();
+        }
+
+// 파일에 데이터를 쓰기
+        FileOutputStream fos = new FileOutputStream(convertFile);
+        fos.write(file.getBytes());
         return Optional.of(convertFile);
     }
 
