2772b62da2c5359a510a026a2b312f8a5c022361
colleenInKorea <92035417+colleenInKorea@users.noreply.github.com>
2023-04-10 20:38:58 +0900

    Update SogonSgonApplication.java

--- src/main/java/com/sparta/sogonsogon/SogonSgonApplication.java
+++ src/main/java/com/sparta/sogonsogon/SogonSgonApplication.java
@@ -6,19 +6,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-//@SpringBootApplication
-//@EnableJpaAuditing
-//@OpenAPIDefinition(servers = {@Server(url = "https://wuzudog.shop", description = "https Server"),@Server(url = "https://sogonsogon.shop", description = "https Server"), @Server(url = "http://localhost:8080", description = "Local Server URL"),@Server(url= "http://43.201.171.120:8080", description = "http서버 입니다. ")})
-//public class SogonSgonApplication {
-//
-//    public static void main(String[] args) {
-//        SpringApplication.run(SogonSgonApplication.class, args);
-//    }
-//
-//}
 @SpringBootApplication
 @EnableJpaAuditing
-@OpenAPIDefinition(servers = {@Server(url = "https://wuzudog.shop", description = "https Server"),@Server(url = "http://localhost:8080", description = "Local Server URL"),@Server(url= "http://15.165.144.179:8080", description = "http서버 입니다. ")})
+@OpenAPIDefinition(servers = {@Server(url = "https://ledgy98.shop", description = "https Server"), @Server(url = "http://localhost:8080", description = "Local Server URL"),@Server(url= "http://52.78.76.85:8080", description = "http서버 입니다. ")})
 public class SogonSgonApplication {
 
     public static void main(String[] args) {
