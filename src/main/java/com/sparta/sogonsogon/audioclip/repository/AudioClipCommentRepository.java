7b0cf129ffe7fde3a3bbc33b76bb28990ac80d97
colleenInKorea <92035417+colleenInKorea@users.noreply.github.com>
2023-04-11 12:49:21 +0900

    design: 오디오 클립 댓글 조회시 pageable 적용 및 추가 출력 데이터 생성

--- src/main/java/com/sparta/sogonsogon/audioclip/repository/AudioClipCommentRepository.java
+++ src/main/java/com/sparta/sogonsogon/audioclip/repository/AudioClipCommentRepository.java
@@ -1,10 +1,12 @@
 package com.sparta.sogonsogon.audioclip.repository;
 
 import com.sparta.sogonsogon.audioclip.entity.Comment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface AudioClipCommentRepository extends JpaRepository<Comment , Long> {
-    List<Comment> findAllByAudioclipId(Long audioclipId);
+    Page<Comment> findAllByAudioclipId(Long audioclipId, Pageable sortedPageable);
 }
