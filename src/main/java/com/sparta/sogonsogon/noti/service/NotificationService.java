1cf99c8341f91f56016d66daf710a641080ae943
Shanyu Li <uhaha529@gmail.com>
2023-04-11 14:54:08 +0900

    docs: apploication 메인서버로 복구

--- src/main/java/com/sparta/sogonsogon/noti/service/NotificationService.java
+++ src/main/java/com/sparta/sogonsogon/noti/service/NotificationService.java
@@ -12,7 +12,6 @@ import com.sparta.sogonsogon.security.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
