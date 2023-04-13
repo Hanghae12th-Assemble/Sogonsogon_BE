23d44d42cbf33711bef76fc77fc280ad9d319dcc
Shanyu Li <uhaha529@gmail.com>
2023-04-13 15:57:47 +0900

    docs:  캐시 제거

--- src/main/java/com/sparta/sogonsogon/noti/service/NotificationService.java
+++ src/main/java/com/sparta/sogonsogon/noti/service/NotificationService.java
@@ -11,14 +11,10 @@ import com.sparta.sogonsogon.security.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
-import org.springframework.data.repository.query.Param;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 import javax.sql.DataSource;
@@ -29,7 +25,6 @@ import java.sql.SQLException;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
-
 import java.util.stream.Collectors;
 
 @Service
@@ -43,10 +38,6 @@ public class NotificationService {
     //DEFAULT_TIMEOUT을 기본값으로 설정
     private static final Long DEFAULT_TIMEOUT = 60 * 60 * 10000L;
     private final DataSource dataSource;
-    // 캐시 이름 설정
-    private static final String NOTIFICATIONS_CACHE_NAME = "notifications";
-    private static final String EXPIRED_NOTIFICATIONS_CACHE_NAME = "expiredNotifications";
-
 
     @Transactional
     public SseEmitter subscribe(UserDetailsImpl userDetails) {
@@ -77,9 +68,7 @@ public class NotificationService {
         Connection con = null;
         try {
             con = dataSource.getConnection();
-            // 데이터소스를 통해 데이터베이스와의 연결을 설정하고 Connection 객체를 생성합니다.
             con.setAutoCommit(false);
-            // 트랜잭션을 수동으로 관리하기 위해 자동 커밋 기능을 false로 설정합니다.
 
             Notification notification = notificationRepository.save(createNotification(receiver, alarmType, message, senderMembername, senderNickname, senderProfileImageUrl));
             String receiverId = String.valueOf(receiver.getId());
@@ -103,7 +92,7 @@ public class NotificationService {
                     con.setAutoCommit(true);
                     // 트랜잭션이 완료되면 자동 커밋 기능을 true로 설정합니다.
                     con.close();
-                    // Connection 객체를 반환하여 커넥션 풀에 반환
+
                 } catch (SQLException e) {
 
                 }
@@ -122,16 +111,7 @@ public class NotificationService {
         }
 
     }
-//    private boolean hasLostData(String lastEventId) {
-//        return !lastEventId.isEmpty();
-//    }
-//    private void sendLostData(String lastEventId, Long memberId, String emitterId, SseEmitter emitter) {
-//        Map<String, Object> eventCaches = emitterRepository.findAllEventCacheStartWithByMemberId(String.valueOf(memberId));
-//
-//        eventCaches.entrySet().stream()
-//                .filter(entry -> lastEventId.compareTo(entry.getKey()) < 0)
-//                .forEach(entry -> sendNotification(emitter, entry.getKey(), emitterId, entry.getValue()));
-//    }
+
     private Notification createNotification(Member receiver, AlarmType alarmType, String message, String senderMembername, String senderNickname, String senderProfileImageUrl) {
         Notification notification = new Notification();
         notification.setReceiver(receiver);
@@ -143,12 +123,6 @@ public class NotificationService {
         return notificationRepository.save(notification);
     }
 
-    //받은 알림 전체 조회, 매번 새로운 쿼리를 수행하지 않도록 캐시를 적용
-    // Spring Cache abstraction을 사용하면 쉽게 캐싱 기능을 구현
-    // 캐시할 메서드에 @Cacheable 애노테이션을 추가하고,
-    // 이 메서드가 실행될 때 캐시를 검색해 존재한다면 결과를 반환하고,
-    // 존재하지 않는다면 새로운 쿼리를 수행한 뒤 결과를 캐시에 저장하는 방식
-    @Cacheable(value = NOTIFICATIONS_CACHE_NAME, key = "#memberId + '-' + #page + '-' + #size")
     @Transactional
     public List<NotificationResponseDto> getAllNotifications(Long memberId, int page, int size) {
         Pageable pageable = PageRequest.of(page, size, Sort.by("createdAt").descending());
@@ -156,20 +130,15 @@ public class NotificationService {
         Page<Notification> notificationPage = notificationRepository.findAllByReceiverIdOrderByCreatedAtDesc(memberId, pageable);
             log.info("알림 최신순으로 페이징 조회했어");
 
-        List<NotificationResponseDto> notificationResponseDtos = notificationPage.stream()
-                .map(NotificationResponseDto::create)
-                .collect(Collectors.toList());
-
-        return notificationResponseDtos;
-    }
-    @Scheduled(cron = "0 0 1 * * ?")
-    @CacheEvict(value = {NOTIFICATIONS_CACHE_NAME, EXPIRED_NOTIFICATIONS_CACHE_NAME}, allEntries = true)
-    public void deleteExpiredNotifications() {
+        // 12시간이 지난 알림 삭제
         LocalDateTime cutoffTime = LocalDateTime.now().minusHours(12);
         List<Notification> expiredNotifications = notificationRepository.findExpiredNotification(cutoffTime);
         for (Notification notification : expiredNotifications) {
             notificationRepository.delete(notification);
         }
+        return notificationPage.stream()
+                .map(NotificationResponseDto::create)
+                .collect(Collectors.toList());
     }
 
     // 특정 회원이 받은 알림을 확인했다는 것을 서비스에 알리는 기능
@@ -177,7 +146,6 @@ public class NotificationService {
     public NotificationResponseDto confirmNotification(Member member, Long notificationId) {
         Notification notification = notificationRepository.findById(notificationId).orElseThrow(
                 () -> new NotFoundException("Notification not found"));
-        // 확인한 유저가 알림을 받은 대상자가 아니라면 예외 발생
         if (!notification.getReceiver().getId().equals(member.getId())) {
             throw new IllegalArgumentException("접근권한이 없습니다. ");
         }
@@ -194,7 +162,6 @@ public class NotificationService {
 
             Notification notification = notificationRepository.findById(notificationId).orElseThrow(
                     () -> new NotFoundException("Notification not found"));
-            // 확인한 유저가 알림을 받은 대상자가 아니라면 예외 발생
             if (!notification.getReceiver().getId().equals(member.getId())) {
                 throw new IllegalArgumentException("접근권한이 없습니다. ");
             }
