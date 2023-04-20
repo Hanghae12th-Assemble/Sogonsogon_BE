4dd44d1ba24ca735d9425cc9a9bfff0e14b8e100
colleenInKorea <92035417+colleenInKorea@users.noreply.github.com>
2023-04-20 13:12:53 +0900

    test: feature/test/member 유저 관련 testCode 작성

---
+++ src/test/java/com/sparta/sogonsogon/member/controller/MemberControllerTest.java
@@ -0,0 +1,40 @@
+package com.sparta.sogonsogon.member.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberControllerTest {
+
+    @Test
+    void signup() {
+    }
+
+    @Test
+    void login() {
+    }
+
+    @Test
+    void updateMemberInfo() {
+    }
+
+    @Test
+    void findbyMembername() {
+    }
+
+    @Test
+    void findListByNickname() {
+    }
+
+    @Test
+    void getMembersBySimilarNickname() {
+    }
+
+    @Test
+    void detailsMember() {
+    }
+
+    @Test
+    void findMemberInfo() {
+    }
+}
